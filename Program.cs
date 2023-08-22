5377c5ed880677a3fa37e9d511cb588fcdb7674f
sananeo0 <sananeo@code.edu.az>
2023-08-22 07:57:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeTask
+{
+    public static class GeometryHelper
+    {
+        public static
+        public static
+        public static
+    }
+
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
